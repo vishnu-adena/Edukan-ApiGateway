598f8657ba9c2ae049cbe8a91b7ad6bc553dd178
vishnu <adenavishnu1@gmail.com>
2024-08-12 16:58:04 +0530

    Update CorsGlobalConfiguration.java

--- src/main/java/com/adena/api_gateway/CorsGlobalConfiguration.java
+++ src/main/java/com/adena/api_gateway/CorsGlobalConfiguration.java
@@ -1,20 +1,20 @@
-package com.adena.api_gateway;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class CorsGlobalConfiguration {
-
-
-//    @Bean
-//    public CorsWebFilter corsFilter() {
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        CorsConfiguration config = new CorsConfiguration();
-//        config.setAllowCredentials(true);
-//        config.addAllowedOriginPattern("http://localhost:*"); // Change this to specific origins for better security
-//        config.addAllowedHeader("*");
-//        config.addAllowedMethod("*");
-//        source.registerCorsConfiguration("/**", config);
-//        return new CorsWebFilter(source);
-//    }
-}
+//package com.adena.api_gateway;
+//
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class CorsGlobalConfiguration {
+//
+//
+////    @Bean
+////    public CorsWebFilter corsFilter() {
+////        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+////        CorsConfiguration config = new CorsConfiguration();
+////        config.setAllowCredentials(true);
+////        config.addAllowedOriginPattern("http://localhost:*"); // Change this to specific origins for better security
+////        config.addAllowedHeader("*");
+////        config.addAllowedMethod("*");
+////        source.registerCorsConfiguration("/**", config);
+////        return new CorsWebFilter(source);
+////    }
+//}
